950453da1c3efa0f1524b33cc705dcdeb86e5b95
hailegithubaccount <michaelhaile1222@gmail.com>
2024-09-30 03:14:04 -0700

    add the new file

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Component/Navbar'
+import Hero from './Component/Hero'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+    </div>
+  )
+}
+
+export default App
